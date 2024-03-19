d768bd5297fa5bf857034d5460d44102d4125607
Linus <linuscodes@scobe.de>
2024-03-19 16:56:01 +0100

    fix combine.sh

--- combine.sh
+++ combine.sh
@@ -1,13 +1,11 @@
-set -ex
-touch "full.md"
-cat header.md > "full.md"
-cat haskell.md >> "full.md"
-cat lambda.md >> "full.md"
-cat types.md >> "full.md"
-cat prolog.md >> "full.md"
-cat unifikation.md >> "full.md"
-cat parallel_processing.md >> "full.md"
-cat java.md >> "full.md"
-cat compiler.md >> "full.md"
+cat header.md > full.md
+cat haskell.md >> full.md
+cat lambda.md >> full.md
+cat types.md >> full.md
+cat prolog.md >> full.md
+cat unifikation.md >> full.md
+cat parallel_processing.md >> full.md
+cat java.md >> full.md
+cat compiler.md >> full.md
 
-pandoc full.md -o full.pdf --toc
+pandoc -o full.pdf -f markdown --toc full.md
