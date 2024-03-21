00eda5c497f6701970397cacdb5bd0169ff4550d
Linus <linuscodes@scobe.de>
2024-03-21 09:56:11 +0100

    move into content folder

--- combine.sh
+++ combine.sh
@@ -1,11 +1,12 @@
 cat header.md > full.md
-cat haskell.md >> full.md
-cat lambda.md >> full.md
-cat types.md >> full.md
-cat prolog.md >> full.md
-cat unifikation.md >> full.md
-cat parallel_processing.md >> full.md
-cat java.md >> full.md
-cat compiler.md >> full.md
+
+cat content/haskell.md >> full.md
+cat content/lambda.md >> full.md
+cat content/types.md >> full.md
+cat content/prolog.md >> full.md
+cat content/unifikation.md >> full.md
+cat content/parallel_processing.md >> full.md
+cat content/java.md >> full.md
+cat content/compiler.md >> full.md
 
 pandoc -H preamble.tex -o full.pdf -f markdown --toc --toc-depth=1 full.md
