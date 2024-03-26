a8b97f53a4eccf85fc908a9c15069448eac7e61c
Linus <linuscodes@scobe.de>
2024-03-26 23:32:25 +0100

    reorder to more closely resemble test order

--- combine.sh
+++ combine.sh
@@ -1,10 +1,10 @@
 cat header.md > full.md
 
 cat content/haskell.md >> full.md
-cat content/lambda.md >> full.md
-cat content/types.md >> full.md
 cat content/prolog.md >> full.md
 cat content/unifikation.md >> full.md
+cat content/lambda.md >> full.md
+cat content/types.md >> full.md
 cat content/parallel_processing.md >> full.md
 cat content/java.md >> full.md
 cat content/compiler.md >> full.md
